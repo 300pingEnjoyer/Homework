a1dacd802309efcafebce03b1072b81b0372592d
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-19 10:06:49 +0700

    task 003 complete

---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("число четное " + num);  
+}
+else
+{
+Console.WriteLine("число нечетное " + num);
+}
+
+
