cf9ee864389e113eb4e01fdfd7020182144dfc50
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-19 09:06:41 +0700

    task 001 complete

---
+++ 001/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("input first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (num1 > num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = num1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(max = num2);
+}
