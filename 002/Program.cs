7cd221a998679e869dc8dbe0d696f5e1878b152a
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-19 09:46:03 +0700

    task 002 complete

---
+++ 002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("input first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input third number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (max < num2)
+    {
+    max = num2;
+    }
+if (max < num3)
+    {
+    max = num3;
+    }
+Console.WriteLine("max = " + max);
