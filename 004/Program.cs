8470fcf2fb095384887c19ee3344854ce61e1c1f
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-19 10:54:08 +0700

    task 004 complete

---
+++ 004/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("input number N");
+int numN = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count < numN)
+{
+    Console.WriteLine(count);
+    count = count + 2;    
+}
